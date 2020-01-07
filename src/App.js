1ba1412b8a8bd0b0a4d5098d0f7626a633dd3bf6
Gustavo_Sanchez <gustavosanchez10059@gmail.com>
2020-01-06 18:09:29 -0600

    Primera version de aprendisaje en ReacJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/*function HelloWorld(proops){
+  console.log(props);
+  return(
+  <div id="hello">
+    <h3>{props.subtitle}</h3>
+    {props.mytext}
+    </div>
+  )
+} */
+class HelloWorld extends React.Component{
+  state = { ///Estado
+    show:true
+  }
+
+  toggleShow = () =>{
+    this.setState({show:!this.state.show})
+  }
+
+  render(){
+    if(this.state.show){
+      return(
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.mytext}
+        <button onClick={ this.toggleShow}>Toggle Show</button>
+      </div>
+      )
+    } else {
+      return <h1>No hay elemntos
+        <button onClick={ this.toggleShow}>Toggle Show</button>
+      </h1>
+    }
+
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      This is my component:
+      <HelloWorld mytext="Hola Mundo" subtitle="skfjlksjd;kfjsdkf"/>
+      <HelloWorld mytext="Tu culo" subtitle="Componente2"/>
+      <HelloWorld mytext="Hola!" subtitle = "Componente 3"/>
+      </div>
   );
 }
+/////Propos
+/* Es es lo mismo que lo de arriba escrito de formas diferentes
+class App extends React.Component{
+  render() {
+    return <div>This is my component <HelloWorld/></div>
+  }
+}
+const App = () => <div>This is my component:<HelloWorld/></div>
 
+*/
 export default App;
